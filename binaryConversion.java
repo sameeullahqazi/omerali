f7352df7030959f5381dc13ed0cfa934c1eb0b7b
Samee Qazi <samee@lamproslabs.com>
2022-07-05 15:37:58 +0500

    Bug fix - missing statement for getting input length

--- binaryConversion.java
+++ binaryConversion.java
@@ -5,7 +5,6 @@ public class binaryConversion {
         int[] denaryPlaceholders = { 128, 64, 32, 16, 8, 4, 2, 1 };
         String binaryPattern = "";
         int denaryNumber = 0;
-        int count = 0;
         
         // Add your code here
         System.out.println("Enter a 8 digit binary pattern");
@@ -14,29 +13,23 @@ public class binaryConversion {
         int lengthBinary = binaryPattern.length();
         while(lengthBinary != 8 ) {
         	System.out.println("Enter a 8 digit binary pattern");
-        	binaryPattern = input.next();}
-        
-        	if(lengthBinary == 8) {
-        		System.out.println("Thank you");
-        		for(int i = 0; i < lengthBinary;i++) {
-        			char bp = binaryPattern.charAt(i);
-        				if(bp == '1') {
-        					denaryNumber += denaryPlaceholders[i];
-        					//count++;
-        					
-        			}
-        				
-        			
-        		
-        	        }
-        	   }  
-        
-        	System.out.println("The denary equivavelent for the binary number" + " " + binaryPattern + " is" + " "+ denaryNumber); 
-        
-        
-	
+        	binaryPattern = input.next();
+        	lengthBinary = binaryPattern.length();
+        }
 
+    	if(lengthBinary == 8) {
+    		System.out.println("Thank you");
+    		for(int i = 0; i < lengthBinary;i++) {
+    			char bp = binaryPattern.charAt(i);
+    				if(bp == '1') {
+    					denaryNumber += denaryPlaceholders[i];
+    					//count++;
+    					
+    			}
+    		}
+    	}  
 
-input.close();
+    	System.out.println("The denary equivavelent for the binary number" + " " + binaryPattern + " is" + " "+ denaryNumber); 
+    	input.close();
     } // End of main program   
 } // End of class
